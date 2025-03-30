06fb635b58f1001c7d9d8c2fa7608d7518663b3a
Hanitha Ganisetti <149133841+hanitha9@users.noreply.github.com>
2025-03-30 16:36:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown("""
     <style>
     /* Full-screen background for first page */
     .input-page {
-        background-image: url('https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80');
+        background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTXRAy6k-QSMzI7SJaqUt7amfFHOprsraWSw&s');
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
